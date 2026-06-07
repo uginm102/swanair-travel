40771df573c03476b06abbb52c76ca017f2b2d4b
Eugene Munyaneza <munyanezae@gmail.com>
2026-06-07 04:57:18 +0200

    Added first page of template.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-//
+import 'bootstrap';
+import '@popperjs/core';
+
+import './main';
+// Initialize after DOM loads
+// feather.replace();
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
+// import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.scss', 'resources/js/app.js'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
                     weights: [400, 500, 600],
                 }),
             ],
-        }),
-        tailwindcss(),
+        })
+        // tailwindcss(),
     ],
     server: {
         watch: {
